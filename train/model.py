526e5fe551d8397863033a4efe7ec5859b0275d9
Ikkei Itoku <ikkeiitoku@gmail.com>
2019-04-26 08:46:29 -0400

    Added more config to ModelCheckpoint

--- train/model.py
+++ train/model.py
@@ -40,9 +40,10 @@ class BaseModel(metaclass=abc.ABCMeta):
         opt = Adam(lr=lr, beta_1=beta_1, beta_2=beta_2, epsilon=epsilon, decay=decay)
         self.model.compile(optimizer=opt, loss='mse')
 
-        mc = ModelCheckpoint(fn_base + '_model.h5', save_best_only=True)
+        mc = ModelCheckpoint(fn_base + '.h5', save_best_only=True, mode='min')
         self.model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size,
                        validation_split=0.1, verbose=1, callbacks=[mc])
+        self.model.save(fn_base + '_final.h5')
 
         df = pd.DataFrame.from_dict(self.model.history.history)
         df.to_csv(fn_base + '_history.csv', index=False)
