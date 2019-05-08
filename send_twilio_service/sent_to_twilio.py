192d1b27ac661a51b127ff53cbfeaa65f1b4509f
ojasklowski <ojasklowski@hioscar.com>
2019-05-07 23:46:58 -0400

    DEEP-66: now passing user phone number and image ID between all microservices.

--- send_twilio_service/sent_to_twilio.py
+++ send_twilio_service/sent_to_twilio.py
@@ -4,17 +4,26 @@ import os
 import urllib
 from urllib import request, parse
 
+from boto3 import resource
+from boto3.dynamodb.conditions import Key, Attr
 
 TWILIO_SMS_URL = "https://api.twilio.com/2010-04-01/Accounts/{}/Messages.json"
 TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
 TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
 
+# The boto3 dynamoDB resource
+dynamodb_resource = resource('dynamodb')
+table = dynamodb_resource.Table('user_image')
+
 
 def lambda_handler(event, context):
+    
+    # Receive message from predict service on the dlresult SNS topic:
     # event_message = event['Records'][0]['Sns']['Message']
     event_message = json.loads(event['Records'][0]['Sns']['Message'])
+    to_number_raw = event_message['phone']
+    to_number = "+" + to_number_raw
     
-    to_number = "+1" + event_message['filename'][-14:-4]
     from_number = os.environ.get("from_number")
     body = "There are " + str(event_message['prediction']) + " people in line right now. Get the Double Shack!!"
 
@@ -30,7 +39,7 @@ def lambda_handler(event, context):
     authentication = "{}:{}".format(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
     base64string = base64.b64encode(authentication.encode('utf-8'))
     req.add_header("Authorization", "Basic %s" % base64string.decode('ascii'))
-
+    
     try:
         # perform HTTP POST request
         with request.urlopen(req, data) as f:
@@ -38,5 +47,5 @@ def lambda_handler(event, context):
     except Exception as e:
         # something went wrong!
         return e
-
+    
     return "SMS sent successfully!"
