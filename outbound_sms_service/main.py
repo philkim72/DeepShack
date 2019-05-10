46b72e03c390ba9434cd852ecff22afc6d2fe916
Phillip Kim <ppk2003@columbia.edu>
2019-05-09 20:59:42 -0400

    DEEP-75: resolve merge conflicts

--- outbound_sms_service/main.py
+++ outbound_sms_service/main.py
@@ -4,7 +4,7 @@ import os
 from urllib import request, parse
 
 from boto3 import resource
-from boto3.dynamodb.conditions import Key, Attr
+# from boto3.dynamodb.conditions import Key, Attr
 
 TWILIO_SMS_URL = "https://api.twilio.com/2010-04-01/Accounts/{}/Messages.json"
 TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
@@ -16,14 +16,14 @@ table = dynamodb_resource.Table('user_image')
 
 
 def lambda_handler(event, context):
-    
+
     # Receive message from predict service on the dlresult SNS topic:
     # event_message = event['Records'][0]['Sns']['Message']
     event_message = json.loads(event['Records'][0]['Sns']['Message'])
 
     to_number_raw = event_message['phone']
     to_number = "+" + to_number_raw
-    
+
     from_number = os.environ.get("from_number")
     body = "There are " + str(event_message['prediction']) + \
         " people in line right now. Get the Double Shack!!"
@@ -40,7 +40,7 @@ def lambda_handler(event, context):
     authentication = "{}:{}".format(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
     base64string = base64.b64encode(authentication.encode('utf-8'))
     req.add_header("Authorization", "Basic %s" % base64string.decode('ascii'))
-    
+
     try:
         # perform HTTP POST request
         with request.urlopen(req, data) as f:
@@ -48,5 +48,5 @@ def lambda_handler(event, context):
     except Exception as e:
         # something went wrong!
         return e
-    
+
     return "SMS sent successfully!"
