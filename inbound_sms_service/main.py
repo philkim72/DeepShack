52ad31666f7ae248da1ec5d0d885b91a0532b0ce
Ikkei Itoku <ikkeiitoku@gmail.com>
2019-05-11 18:05:12 -0400

    DEEP-79 Some cleanup

--- inbound_sms_service/main.py
+++ inbound_sms_service/main.py
@@ -22,19 +22,23 @@ def lambda_handler(event, context):
     outbound_message = {'phone_number': "+" + event['From'].split("B")[-1]}
     message_content = event['Body'].strip()
     if message_content not in ('0', '1'):
-        text_message = ("Hello from DeepShack!\n\nDeepShack Menu:\n0 - About the Service\n"
+        text_message = ("Hello from DeepShack!\n\n"
+                        "DeepShack Menu:\n"
+                        "0 - About the Service\n"
                         "1 - How long is the line at Shake Shack?")
         outbound_message['body'] = text_message
         publish_message(outbound_message, SMS_TOPIC_ARN)
     else:
         if message_content == '0':
-            text_message = ("A Deep Learning application that estimates the Shake Shack line "
-                            "using the live streaming located at Madison Square Park. The "
-                            "application is based on a decoupled microservice architecture "
-                            "implemented using AWS ECS, Lambda, SNS and Twilio SMS.")
+            text_message = ("A Deep Learning application that estimates the "
+                            "Shake Shack line using the live streaming "
+                            "located at Madison Square Park. The application "
+                            "is based on a decoupled microservice "
+                            "architecture implemented using AWS ECS, Lambda, "
+                            "SNS and Twilio SMS.")
             outbound_message['body'] = text_message
             publish_message(outbound_message, SMS_TOPIC_ARN)
-        else:
+        else == '1':
             publish_message(outbound_message, SCRAPE_TOPIC_ARN)
             text_message = ("Hello from DeepShack! We are calculating your "
                             "time-to-burger. Hang tight...")
