4bb9e45225c98d899c0f2985382563a5c2754ddf
diug85 <diug85@yahoo.com.mx>
2019-05-11 20:37:25 -0400

    DEEP-92 fix bug in predict-trigger service and ECS

--- predict_trigger_service/main.py
+++ predict_trigger_service/main.py
@@ -5,20 +5,10 @@ import boto3
 
 logging.getLogger().setLevel(logging.INFO)
 
-SMS_TOPIC_ARN = 'arn:aws:sns:us-east-1:245636212397:triggerSMS'
-
-def publish_message(message, topic_arn):
-    """Publishes a message to SNS topic"""
-    sns = boto3.client('sns')
-    response = sns.publish(
-        TopicArn=topic_arn,
-        Message=json.dumps({'default': json.dumps(message)}),
-        MessageStructure='json'
-    )
-    logging.info(response) 
 
 def predict_trigger_handler(event, context):
     """Subscribes to triggerPredict SNS topic and triggers"""
+    # inbound_message = event['Records'][0]['Sns']['Message']
     inbound_message = json.loads(event['Records'][0]['Sns']['Message'])
     filename = inbound_message['filename']
     phone_number = inbound_message['phone_number']
@@ -27,10 +17,10 @@ def predict_trigger_handler(event, context):
     response = ecs.run_task(
         cluster='cluster2',
         launchType='FARGATE',
-        taskDefinition='predict_task',
+        taskDefinition='predict_task2',
         count=1,
         overrides={'containerOverrides': [{
-            'name': 'deepshack_predict_container',
+            'name': 'deepshack_predict',
             'environment': [{'name': 'IMAGE_ID', 'value': filename},
                             {'name': 'PHONE', 'value': phone_number}]
         }]},
@@ -42,14 +32,6 @@ def predict_trigger_handler(event, context):
             }
         }
     )
-    
-    outbound_message = {'filename': filename, 'phone_number': phone_number}
-    outbound_message['body'] = (
-                "We are now spinning up an ECS instance. "
-                "This machine will count the people in the Shack line. "
-                "This could take up to a minute."
-    )
-    publish_message(outbound_message, SMS_TOPIC_ARN)
 
     response = json.dumps(response, sort_keys=True, default=str)
     logging.info(response)
